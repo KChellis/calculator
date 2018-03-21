52beb4d52175a7a2fc38e29bf94b1d0d9f8dd4d5
Aaron Akbari-Mort and Kristen Chellis <kristen@>
2018-03-21 13:53:53 -0700

    add user interface for subract divide and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,34 @@ var divide = function(number1, number2) {
 $(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseFloat($("#add1").val());
+    var number2 = parseFloat($("#add2").val());
 
-    var result = add(number1, number2);
+    var result = add(number1, number2).toFixed(2);
     $(".result").text(number1 + "+" + number2 + "=" + result);
+  });
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseFloat($("#sub1").val());
+    var number2 = parseFloat($("#sub2").val());
 
+    var result = subtract(number1, number2).toFixed(2);
+    $(".result").text(number1 + "-" + number2 + "=" + result);
+  });
+  $("form#mult").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseFloat($("#mult1").val());
+    var number2 = parseFloat($("#mult2").val());
+
+    var result = multiply(number1, number2).toFixed(2);
+    $(".result").text(number1 + "*" + number2 + "=" + result);
+  });
+  $("form#divi").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseFloat($("#divi1").val());
+    var number2 = parseFloat($("#divi2").val());
+
+    var result = divide(number1, number2).toFixed(2);
+    $(".result").text(number1 + "/" + number2 + "=" + result);
   });
 });
